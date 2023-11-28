fbfb764324ac965ec2c5c1ac50ddc3a49f98a4a4
sanguine-dreams <65179987+sanguine-dreams@users.noreply.github.com>
2023-11-29 02:58:03 +0300

    fix attempt: background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Intro from "./sections/Intro";
 import AboutMe from "./sections/AboutMe";
-
 import Skills from "./sections/Skills";
 import NavBar from "./sections/NavBar";
 import Footer from "./sections/Footer";
