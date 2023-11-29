939b0792b1de9ae5c80cf597944cb624e7e75692
sanguine-dreams <65179987+sanguine-dreams@users.noreply.github.com>
2023-11-29 23:05:35 +0300

    add: Display Image

---
+++ src/components/DisplayImage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import dp2 from "../assets/dp2.png";
+
+function DisplayImage() {
+  return (
+    <div className="display-image">
+      <img src={dp2} alt="dp" id="dp" />
+    </div>
+  );
+}
+
+export default DisplayImage;
--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -1,6 +1,9 @@
+import DisplayImage from "../components/DisplayImage";
+
 function Intro(){
     return <div className="intro">
-        <h1>Welcome to My portfolio: As you can see, it's under construction. </h1>
+        <DisplayImage />
+        <h1 className="intro-text" >Welcome to My portfolio: As you can see, it's under construction. </h1>
     </div>
 }
 
